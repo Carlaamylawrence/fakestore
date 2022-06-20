cf75512e13c74da183a3039e072314e7ea172576
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-20 16:31:10 +0200

    javascript edits

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-fetch("https://fakestoreapi.com/products")
+const url = "https://fakestoreapi.com/products";
+let items = [];
+
+fetch(url)
   .then((res) => res.json())
-  .then((json) => console.log(json));
+  .then((data) => {
+    console.log(data);
+    items = data;
+    document.querySelector("#clothes").innerHTML = `${items}`;
+    showItems(items);
+  });
+
+function showItems(items) {
+  document.querySelector("#clothes").innerHTML = "";
+  items.forEach((items) => {
+    document.querySelector("#clothes").innerHTML += `${items}`;
+  });
+}
+
+// function addItems() {
+//   const item = document.querySelector("#addProduct").value;
+//   if (item === "")
+//     document.querySelector("#products").innerHTML = `ENTER VALID VALUE`;
+//   else {
+//     items.push(item);
+//     showItems(items);
+//   }
+// }

a1f76a22d52b6edaec18e21454e1b4e4cebde2aa
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-21 00:42:43 +0200

    add items broken, need to add modal

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const url = "https://fakestoreapi.com/products";
 let items = [];
 
-fetch(url)
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     items = data;
+//     document.querySelector("#clothes").innerHTML = `${items}`;
+//     showItems(items);
+//   });
+
+  fetch(url)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     items = data;
-    document.querySelector("#clothes").innerHTML = `${items}`;
-    showItems(items);
+    console.log(data)
+    for (let i=0; i < items.length; i++){
+      document.querySelector("#clothes").innerHTML += `
+      <img onClick="toggleActive('#item${i}')" src=${items[i].image} style="width:200px; height:200px; padding:20px; ">
+      <div class="modal" id="item${i} >
+      <button onClick="toggleActive('#item${i}')">x</button>
+      ${items[i].category}
+      `
+    }
   });
 
-function showItems(items) {
-  document.querySelector("#clothes").innerHTML = "";
-  items.forEach((items) => {
-    document.querySelector("#clothes").innerHTML += `${items}`;
-  });
-}
+  function toggleActive(el) {
+    document.querySelector(el).classList.toggle("active");
+  }
 
-// function addItems() {
-//   const item = document.querySelector("#addProduct").value;
-//   if (item === "")
-//     document.querySelector("#products").innerHTML = `ENTER VALID VALUE`;
-//   else {
-//     items.push(item);
-//     showItems(items);
-//   }
+// function showItems(items) {
+//   document.querySelector("#clothes").innerHTML = "";
+//   items.forEach((items) => {
+//     document.querySelector("#clothes").innerHTML += `${items}`;
+//   });
 // }
+
+function addItems() {
+  const product = document.querySelector("#addItems").value;
+  let item = {
+    category : items.category,
+    description :  items.description,
+    productId : items.id,
+    image : items.image,
+    price:  items.price,
+    rating : items.rating.rate,
+    title: items.title
+  }
+  if (item === "")
+    document.querySelector("#clothes").innerHTML = `ENTER VALID VALUE`;
+  else {
+    items.push(item);
+    showItems(items);
+  }
+}

e2238dced6e1eb0b7c5dc853b85d7d85cb01d60c
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-21 09:06:08 +0200

    fixed item add

--- index.js
+++ index.js
@@ -10,47 +10,40 @@ let items = [];
 //     showItems(items);
 //   });
 
-  fetch(url)
+fetch(url)
   .then((res) => res.json())
   .then((data) => {
     items = data;
-    console.log(data)
-    for (let i=0; i < items.length; i++){
-      document.querySelector("#clothes").innerHTML += `
-      <img onClick="toggleActive('#item${i}')" src=${items[i].image} style="width:200px; height:200px; padding:20px; ">
-      <div class="modal" id="item${i} >
-      <button onClick="toggleActive('#item${i}')">x</button>
-      ${items[i].category}
-      `
-    }
+    console.log(data);
+    showItems(data);
+    // for (let i = 0; i < items.length; i++) {
+    //   document.querySelector("#clothes").innerHTML += `
+    //   <img onClick="toggleActive('#item${i}')" src=${items[i].image} style="width:200px; height:200px; padding:20px; ">
+
+    //   `;
+    // }
   });
 
-  function toggleActive(el) {
-    document.querySelector(el).classList.toggle("active");
-  }
-
-// function showItems(items) {
-//   document.querySelector("#clothes").innerHTML = "";
-//   items.forEach((items) => {
-//     document.querySelector("#clothes").innerHTML += `${items}`;
-//   });
-// }
+function showItems(items) {
+  document.querySelector("#clothes").innerHTML = "";
+  items.forEach((item, i) => {
+    document.querySelector(
+      "#clothes"
+    ).innerHTML += ` <img onClick="toggleActive('#item${i}')" src=${item.image} style="width:200px; height:200px; padding:20px; ">`;
+  });
+}
 
 function addItems() {
-  const product = document.querySelector("#addItems").value;
   let item = {
-    category : items.category,
-    description :  items.description,
-    productId : items.id,
-    image : items.image,
-    price:  items.price,
-    rating : items.rating.rate,
-    title: items.title
-  }
-  if (item === "")
-    document.querySelector("#clothes").innerHTML = `ENTER VALID VALUE`;
-  else {
-    items.push(item);
-    showItems(items);
-  }
+    category: document.querySelector("#category").value,
+    description: document.querySelector("#description").value,
+    productId: document.querySelector("#productId").value,
+    image: document.querySelector("#image").value,
+    price: document.querySelector("#price").value,
+    rating: document.querySelector("#rating").value,
+    title: document.querySelector("#title").value,
+  };
+  items.push(item);
+  showItems(items);
+  console.log(items);
 }

9b715b880e992906251efa5315ff714cc1b16288
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-22 08:45:02 +0200

    added sort by name

--- index.js
+++ index.js
@@ -52,3 +52,16 @@ const sortedPrice = (e) => {
     showItems(sorted.reverse());
   }
 };
+
+const sortedName = (e) => {
+  const direction = e.target.value;
+  const sorted = items.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+};
